b6e4052c40b859c099efd7b3c3baaa25791fea1a
Bill Rashid <bill.rashid12@gmail.com>
2019-01-01 22:22:05 -0600

    gettin server to work

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon('exam-facade' + '/build/favicon.ico'));
+// the exam-facade is the current directory from where the script is running
+app.use(express.static('exam-facade'));
+app.use(express.static(path.join('exam-facade', 'build')));
 app.get('/ping', function(req, res) {
   return res.send('pong');
 });
